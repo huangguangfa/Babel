2077c07618fcb255c3a54faaecce68eca27560bf
hgf <1454556135@qq.com>
2021-07-23 00:55:25 +0800

    [feat]理解常见ASTAST节点、并且在traverse里面配置节点监听信息

--- src/examples/code.js
+++ src/examples/code.js
@@ -1,7 +1,12 @@
 let a = 1;
 let b = 2;
 let arr = [1,2,3];
+// let ss = 111;
 function test(){
-    return a + b;
+    console.info(a + b);
 }
 console.log(a);
+
+export function getName(){
+    console.log(11)
+}
--- src/examples/index.js
+++ src/examples/index.js
@@ -4,16 +4,31 @@ let sourceCode = fsReadFileSync('./code.js');
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 const generate = require("@babel/generator").default;
-
+const types = require("@babel/types")
 const ast = parser.parse(sourceCode,{
     sourceType:"unambiguous"
 })
-
 traverse(ast, {
-    enter(path,state){
-        console.log('aaaa',path)
+    CallExpression(path,state){
+        // const calleeName = generate(path.node.callee).code;
+        // console.log(path.node.callee)
+        // if ( types.isMemberExpression(path.node.callee) 
+        //     && path.node.callee.object.name === 'console' 
+        //     && ['log', 'info', 'error', 'debug'].includes(path.node.callee.property.name) 
+        //    ) {
+        //     const { line, column } = path.node.loc.start;
+        //     path.node.arguments.unshift(types.stringLiteral(`filename: (${line}, ${column})`))
+        // }
     },
-    ArrayExpression(a, state) {
-        console.log(a)
+    // FunctionDeclaration(path, state){
+    //     console.log(path.node)
+    // },
+    // VariableDeclaration(path){
+    //     num ++
+    //     // console.log(path.node)
+    // }
+    ExportNamedDeclaration(path){
+        console.log(path.node)
     }
 });
+
